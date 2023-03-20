82ebdefbda987bc93aced367380481b6df8c6f4b
lina-ejb <linakryzneva@gmail.com>
2023-03-20 17:06:04 +0300

    create divide and conquer

---
+++ 7-kyu/Divide_and_Conquer.js
@@ -0,0 +1,14 @@
+/*
+Description:
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+Return as a number.
+*/
+
+function divCon(x) {
+    let result = 0;
+      for (let i = 0; i < x.length; i++) {
+        typeof x[i] === 'string' ? result=result - x[i] : result+=x[i] 
+      }
+     return result;
+    } 
