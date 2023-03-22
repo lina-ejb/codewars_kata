1a2d6028e49c065273dea7df549b6e8d98190d48
lina-ejb <linakryzneva@gmail.com>
2023-03-22 13:27:27 +0300

    convert number to array task

---
+++ 8-kyu/convert number to array.js
@@ -0,0 +1,12 @@
+/*
+Description:
+Given a random non-negative number, you have to return the digits of this number 
+within an array in reverse order.
+*/
+
+
+function digitize(n) {
+    let digits = String(n).split('').map(Number).reverse()
+      return digits
+  }
+
